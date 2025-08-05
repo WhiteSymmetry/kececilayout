6eeae326f0f1205efc2935e3acfb13661745a6a9
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-08-05 10:25:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kececilayout",
-    version="0.2.4",
+    version="0.2.5",
     description="A deterministic node placement algorithm used in graph visualization. In this layout, nodes are arranged sequentially along a defined primary axis. Each subsequent node is then alternately offset along a secondary, perpendicular axis, typically moving to one side of the primary axis and then the other. Often, the magnitude of this secondary offset increases as nodes progress along the primary axis, creating a characteristic zig-zag or serpentine pattern.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -20,7 +20,7 @@ setup(
         "networkit",
         "graphillion",
         "pycairo" ,
-        "cairocffi",
+        "cairocffi"
     ],
     extras_require={
 
@@ -28,8 +28,9 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent"
     ],
     python_requires='>=3.9',
     license="MIT",
 )
+
