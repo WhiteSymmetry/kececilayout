09a53ab828f692218a6c526455ee9129832a15a4
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-08-09 00:43:09 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,25 +1,42 @@
-# Project Information
+# Configuration file for the Sphinx documentation builder.
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))  # kececilayout modülünü dahil et
+
 project = 'kececilayout'
-author = 'Mehmet Keçeci'
 copyright = '2025, Mehmet Keçeci'
+author = 'Mehmet Keçeci'
+release = '0.2.6'
 
-# Version Management
-# from setuptools_scm import get_version
-# version = get_version(root='..', relative_to=__file__)
-version = '0.2.6'  # Replace with your actual version number
-release = version
-
-# General Configuration
-master_doc = 'index'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',  # Google/NumPy style docstrings
+    'sphinx.ext.intersphinx',
+    'sphinx_rtd_theme',
 ]
+
+templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# HTML Output Configuration
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = '_static/logo.png'  # Optional: Add your project logo
-html_favicon = '_static/favicon.ico'  # Optional: Add a favicon
+html_logo = "_static/logo.png"
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'logo_only': True,
+}
+
+# intersphinx mapping
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'networkx': ('https://networkx.org/documentation/stable/', None),
+    'matplotlib': ('https://matplotlib.org/stable/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'igraph': ('https://igraph.org/python/doc/', None),
+    'rustworkx': ('https://qiskit.org/rustworkx/', None),
+    'networkit': ('https://networkit.github.io/documentation/', None),
+    'graphillion': ('https://graphillion.readthedocs.io/en/latest/', None),
+}
