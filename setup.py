9fa2383f9bfaf71c0563112e02111e521eca2841
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-08-09 02:39:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup, find_packages
 
+# __version__'ı __init__.py'den oku
+def get_version():
+    with open('kececilayout/__init__.py', 'r') as f:
+        content = f.read()
+    match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content, re.M)
+    if match:
+        return match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 setup(
     name="kececilayout",
-    version="0.2.7",
+    #version="0.2.7",
+    version=get_version(),
     description="A deterministic node placement algorithm used in graph visualization. In this layout, nodes are arranged sequentially along a defined primary axis. Each subsequent node is then alternately offset along a secondary, perpendicular axis, typically moving to one side of the primary axis and then the other. Often, the magnitude of this secondary offset increases as nodes progress along the primary axis, creating a characteristic zig-zag or serpentine pattern.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -29,3 +39,4 @@ setup(
     python_requires='>=3.9',
     license="MIT",
 )
+
