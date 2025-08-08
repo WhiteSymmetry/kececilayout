b16f81cca4e50fba49b2f31f6672df80ef25345f
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-08-09 01:21:14 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,30 +8,23 @@ copyright = '2025, Mehmet Keçeci'
 author = 'Mehmet Keçeci'
 release = '0.2.7'
 
-autodoc_mock_imports = [
-    "igraph",
-    "networkit",
-    "rustworkx",
-    "graphillion",
-]
+# Version Management
+# from setuptools_scm import get_version
+# version = get_version(root='..', relative_to=__file__)
+version = '0.2.7'  # Replace with your actual version number
+release = version
 
+# General Configuration
+master_doc = 'index'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',  # Google/NumPy style docstrings
-    'sphinx.ext.intersphinx',
-    'sphinx_rtd_theme',
+    'sphinx.ext.napoleon'
 ]
-
-templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# HTML Output Configuration
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = "_static/logo.png"
-html_theme_options = {
-    'navigation_depth': 4,
-    'collapse_navigation': False,
-    'sticky_navigation': True,
-    'logo_only': True,
-}
+html_logo = '_static/logo.png'  # Optional: Add your project logo
+html_favicon = '_static/favicon.ico'  # Optional: Add a favicon
