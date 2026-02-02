ec25b25cf9363598c30ae646a86d9a928bb7d21c
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-02-02 11:43:50 +0300

    Update __init__.py

--- kececilayout/__init__.py
+++ kececilayout/__init__.py
@@ -10,7 +10,7 @@ import inspect
 import warnings
 
 # Paket sürüm numarası
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 # =============================================================================
 # OTOMATİK İÇE AKTARMA VE __all__ OLUŞTURMA
@@ -231,6 +231,7 @@ def old_function_placeholder():
 
 # Eğer bu eski fonksiyonu da dışa aktarmak istiyorsanız, __all__ listesine ekleyin
 # __all__.append('old_function_placeholder')
+
 
 
 
