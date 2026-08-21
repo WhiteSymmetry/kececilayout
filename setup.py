878a4abc41475a151192f733bea262938d80be30
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-08-21 15:02:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ setup(
     long_description_content_type="text/markdown", # Bu satır da önemlidir
     author="Mehmet Keçeci",
     maintainer="Mehmet Keçeci",
-    author_email="bilginomi@yaani.com",
-    maintainer_email="bilginomi@yaani.com",
+    author_email="enfo@tuta.io",
+    maintainer_email="enfo@tuta.io",
     url="https://github.com/WhiteSymmetry/kececilayout",
     packages=find_packages(),
     install_requires=[
